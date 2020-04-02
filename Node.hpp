9af0b13539ebd0f0640e5ab7eb6d95a35c59cd9c
sarahn20 <56127151+sarahn20@users.noreply.github.com>
2020-04-02 03:39:46 -0500

    Create Node.hpp

---
+++ Node.hpp
@@ -0,0 +1,18 @@
+#ifndef Node_hpp
+#define Node_hpp
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        Node(string payload);
+        string getPayload();
+        Node* getNextNode();
+        void setNextNode(Node* n);
+
+    private:
+        string payload;
+        Node* nextNode;
+};
+#endif
