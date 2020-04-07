a88a51c4919c3e8a9cfcad9245894d510d3287a2
sarahn20 <56127151+sarahn20@users.noreply.github.com>
2020-04-07 04:35:02 -0500

    Assignment15 Updates

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
 #include "StringFactory.hpp"
+#include "Queue.hpp"
+#include "Stack.hpp"
+#include "ShuntingYard.hpp"
 
 using namespace std;
 
 int main()
 {
     string s = "123+42-8*2";
-    string ops = "+-*/";
+    string ops = "+-*/()";
     Queue* inputQ = StringFactory::split(s,ops); 
+    inputQ->display();
+
+    Queue* outputQ = ShuntingYard::process(inputQ);
+    outputQ->display();
+
 }
