4054038319d2a243eb9275ac52cd51541d99eb4b
sarahn20 <56127151+sarahn20@users.noreply.github.com>
2020-04-07 04:28:21 -0500

    Assignment15 Updates

--- Queue.hpp
+++ Queue.hpp
@@ -1,18 +1,19 @@
 #ifndef Queue_hpp
 #define Queue_hpp
 #include "Node.hpp"
-
 class Queue
 {
-private:
-    Node* head;
-    int count;
-    Node* runToEndOfList();
-    
-public:
-    Queue();
-    void enqueue(string payload);
-    string dequeue();
-    void display();
+    private:
+        Node* head;
+        Node* tail;
+        int count;
+
+    public:
+        Queue();
+        void enqueue(string payload);
+        string dequeue();
+        void display();
+        int getCount();
+        Node* getHead();
 };
 #endif
